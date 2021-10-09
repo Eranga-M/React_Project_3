a745516ddf75a1fc36f048ad93f3a0030579ff2e
Eranga Ruwanhewa <eranga.ruwanhewa@gmail.com>
2021-10-09 14:25:09 +0530

    Third React Project

---
+++ src/Components/header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+    <header>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    );
+}
+
+export default Header;
---
+++ src/Components/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SiteInfo = () => {
+    return(
+        <div className="site-info">
+            <div className="title">
+                Coffeedo
+            </div>
+            <div className="sub-title">
+                Cake | Coffee
+            </div>
+        </div>
+    );
+}
+
+export default SiteInfo;
